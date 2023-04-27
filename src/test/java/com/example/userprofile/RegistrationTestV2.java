53cc81766ca9119f39cd21ee9b3402055c1a6faa
Nidhi Dutta <nidhi2627@gmail.com>
2023-04-27 20:55:26 +0000

    added test for createUser duplicate failure

--- src/test/java/com/example/userprofile/RegistrationTestV2.java
+++ src/test/java/com/example/userprofile/RegistrationTestV2.java
@@ -2,7 +2,7 @@ package com.example.userprofile;
 
 import com.example.userprofile.utils.UserProfileUtils;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -12,8 +12,6 @@ import java.io.IOException;
 import java.util.UUID;
 
 class RegistrationTestV2 {
-
-	private static RequestSpecification requestSpec;
 
 	@BeforeClass
 	public static void setUp() throws IOException {
@@ -29,6 +27,22 @@ class RegistrationTestV2 {
 		Assert.assertEquals(response.getStatusCode(), 201);
 		Assert.assertEquals(response.path("name"), "testName" + uuid);
 		Assert.assertEquals(response.path("email"), "testEmail" + uuid + "@gmail.com");
+
+		// tear down test data
+		UserProfileUtils.tearDown(id, "public/v2/");
+	}
+
+	@Test
+	public void createUserDuplicateFailure() throws JSONException {
+		String uuid = UUID.randomUUID().toString();
+		String id = UserProfileUtils.createUser(uuid, "public/v2/").path("id").toString();
+
+		Response response = UserProfileUtils.createUser(uuid, "public/v2/");
+
+		JSONArray jsonResponseArray = new JSONArray(response.body().asString());
+		Assert.assertEquals(response.getStatusCode(), 422);
+		Assert.assertEquals(jsonResponseArray.getJSONObject(0).getString("field"), "email");
+		Assert.assertEquals(jsonResponseArray.getJSONObject(0).getString("message"), "has already been taken");
 
 		// tear down test data
 		UserProfileUtils.tearDown(id, "public/v2/");
