7c47faf5da772becb51ebbf4e5e60b5c57a7b2a0
Nidhi Dutta <nidhi2627@gmail.com>
2023-04-28 17:10:05 +0000

    added tests for getUser API Version 1

---
+++ src/test/java/com/example/userprofile/RegistrationTestV1.java
@@ -0,0 +1,48 @@
+package com.example.userprofile;
+
+import com.example.userprofile.utils.UserProfileUtils;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.UUID;
+
+class RegistrationTestV1 {
+    private RequestSpecification requestSpec;
+    private static String versionPath;
+
+    @BeforeClass
+    private void setUp() {
+        requestSpec = UserProfileUtils.setUpRequestSpec();
+        versionPath = "public/v1/";
+    }
+
+    @Test
+    public void getUserDetailsSuccess() throws JSONException {
+        String uuid = UUID.randomUUID().toString();
+        String id = UserProfileUtils.createUser(uuid, requestSpec, versionPath).path("data.id").toString();
+
+        Response response = UserProfileUtils.readUser(id, requestSpec, versionPath);
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertEquals(response.path("data.id").toString(), id);
+        Assert.assertEquals(response.path("data.name"), "testName" + uuid);
+        Assert.assertEquals(response.path("data.email"), "testEmail" + uuid + "@gmail.com");
+
+        // tear down test data
+        UserProfileUtils.tearDown(id, requestSpec, versionPath);
+    }
+
+    @Test
+    public void getUserDetailsNotFound() {
+        String uuid = UUID.randomUUID().toString();
+        Response response = UserProfileUtils.readUser(uuid, requestSpec, versionPath);
+
+        Assert.assertEquals(response.getStatusCode(), 404);
+        Assert.assertEquals(response.path("data.message"), "Resource not found");
+    }
+
+}
