bc0bf1aff29e16641b69e9ae00da22caec395920
Nidhi Dutta <nidhi2627@gmail.com>
2023-04-28 13:40:12 +0000

    parametrized versionPath

--- src/test/java/com/example/userprofile/RegistrationTestV2.java
+++ src/test/java/com/example/userprofile/RegistrationTestV2.java
@@ -15,16 +15,18 @@ import java.util.UUID;
 class RegistrationTestV2 {
 
 	private RequestSpecification requestSpec;
+	private static String versionPath;
 
 	@BeforeClass
 	public void setUp() throws IOException {
 		requestSpec = UserProfileUtils.setUpRequestSpec();
+		versionPath = "public/v2/";
 	}
 
 	@Test
 	public void createUserSuccess() throws JSONException {
 		String uuid = UUID.randomUUID().toString();
-		Response response = UserProfileUtils.createUser(uuid, requestSpec, "public/v2/");
+		Response response = UserProfileUtils.createUser(uuid, requestSpec, versionPath);
 		String id = response.path("id").toString();
 
 		Assert.assertEquals(response.getStatusCode(), 201);
@@ -32,15 +34,15 @@ class RegistrationTestV2 {
 		Assert.assertEquals(response.path("email"), "testEmail" + uuid + "@gmail.com");
 
 		// tear down test data
-		UserProfileUtils.tearDown(id, requestSpec, "public/v2/");
+		UserProfileUtils.tearDown(id, requestSpec, versionPath);
 	}
 
 	@Test
 	public void createUserDuplicateFailure() throws JSONException {
 		String uuid = UUID.randomUUID().toString();
-		String id = UserProfileUtils.createUser(uuid, requestSpec, "public/v2/").path("id").toString();
+		String id = UserProfileUtils.createUser(uuid, requestSpec, versionPath).path("id").toString();
 
-		Response response = UserProfileUtils.createUser(uuid, requestSpec, "public/v2/");
+		Response response = UserProfileUtils.createUser(uuid, requestSpec, versionPath);
 
 		JSONArray jsonResponseArray = new JSONArray(response.body().asString());
 		Assert.assertEquals(response.getStatusCode(), 422);
@@ -48,15 +50,15 @@ class RegistrationTestV2 {
 		Assert.assertEquals(jsonResponseArray.getJSONObject(0).getString("message"), "has already been taken");
 
 		// tear down test data
-		UserProfileUtils.tearDown(id, requestSpec, "public/v2/");
+		UserProfileUtils.tearDown(id, requestSpec, versionPath);
 	}
 
 	@Test
 	public void getUserDetailsSuccess() throws JSONException {
 		String uuid = UUID.randomUUID().toString();
-		String id = UserProfileUtils.createUser(uuid, requestSpec, "public/v2/").path("id").toString();
+		String id = UserProfileUtils.createUser(uuid, requestSpec, versionPath).path("id").toString();
 
-		Response response = UserProfileUtils.readUser(id, requestSpec, "public/v2/");
+		Response response = UserProfileUtils.readUser(id, requestSpec, versionPath);
 
 		Assert.assertEquals(response.getStatusCode(), 200);
 		Assert.assertEquals(response.path("id").toString(), id);
@@ -64,7 +66,7 @@ class RegistrationTestV2 {
 		Assert.assertEquals(response.path("email"), "testEmail" + uuid + "@gmail.com");
 
 		// tear down test data
-		UserProfileUtils.tearDown(id, requestSpec, "public/v2/");
+		UserProfileUtils.tearDown(id, requestSpec, versionPath);
 	}
 
 }
